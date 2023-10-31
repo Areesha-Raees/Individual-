a29a8d054ebe65c6197cf3f664877887621cbc23
Mr5zy <mirzauraiboo7@gmail.com>
2023-10-31 15:25:39 +0500

    Updated

---
+++ Test Suites/Untitled Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Untitled Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0b0f9cd3-eff7-4935-ac8b-0ef8e4baae8a</testSuiteGuid>
+   <testCaseLink>
+      <guid>e7e30934-d34b-4ef9-9587-bfb862e1b06e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Untitled Test Suite/Untitled Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
